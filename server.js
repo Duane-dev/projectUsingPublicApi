2b76a23c4b5976fc6d916cc0992b952073ef5fd7
Duane-dev <correa1800468@mls.ceu.edu.ph>
2022-05-31 19:46:19 +0800

    initial commit

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/project-using-fake-api'));
+app.use(express.static('./dist/project-using-public-api'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/project-using-fake-api/'}),
+    res.sendFile('index.html', {root: 'dist/project-using-public-api/'}),
 );
 
 // Start the app by listening on the default Heroku port
